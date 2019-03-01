17ccf3d649b8ad8733d217249c06c57cb22deac1
Zachary Taylor <zlokquay@gmail.com>
2019-03-01 09:44:32 -0500

    oh god please help

--- Driver.py
+++ Driver.py
@@ -1,63 +1,47 @@
-#Sinclair Fuh, Zachary Taylor, Lron Coop
+#Sinclair Fuh, Zachary Taylor, Thomas Cooper
 #COSC 370, Artificial Intelligence
 
 import random
 
 queenBoard = []
-indexList = []
+board = []
 tempBoard = []
 
-#Some help from https://stackoverflow.com/questions/33811240/python-randomly-fill-2d-array-with-set-number-of-1s
-#To help with randomly populating th board efficiently. queenBoard[i:i+n] fills in the next n spots
-#with the next n positions from the 1d array, then makes a new index and inserts the next n. Doing this
-#helps convert the shuffled 1d array into a shuffled 2d array
+#sets up a 1d array of queen positions.
 def createBoard(n):
   global queenBoard
-  queenBoard = ([1] * n) + ([0] * ((n*n) - n))
+  for i in range(n):
+    queenBoard.append(i)
   random.shuffle(queenBoard)
-  queenBoard = [queenBoard[i:i+n] for i in range(0, n*n, n)]
- 
-#Used to find the positions of queens on the board and append them to a list.
-#Simple way to work with only the queens positions for the queenHeuristic
-def indices():
-  global tempBoard
-  for i in range(len(tempBoard)):
-    for j in range(len(tempBoard[i])):
-      if tempBoard[i][j] == 1:
-        indexList.append([i,j])
 
-#A heurstic funtion to return the number of conflicts a queen has with other queens on the board.
-#Queens with more conflicts are considered first when attempting a move. INCOMPLETE
-def queenHeuristic(lst):
+
+#Checks whole board for conflicts with queens to return the value of all conflicts on the board.
+def queenHeuristic(board):
   conflicts = 0
-  currentCheck = 0
-  for i in range(len(lst)):
-    currentCheck = lst[i]
-    for j in range(len(lst)):
-      if currentCheck == lst[j]:
-        continue
-      elif currentCheck[0] == lst[j][0]:
-        conflicts = conflicts + 1
-      elif currentCheck[1] == lst[j][1]:
-        conflicts = conflicts + 1
-      elif abs(currentCheck[0] - lst[j][0]) == abs(currentCheck[1] - lst[j][1]):
-        conflicts = conflicts + 1
-
-
-#def hillClimbing(lst):
-    
-
+  for i in board:
+    for j in board:
+      if board[i] == board[j]:
+        conflicts += 1
+      elif abs(j - i) == abs(board[j] - board[i]):
+        conflicts += 1
+  return conflicts
+  
 #Main program begins here
 print("1.) n-Queens\n2.) Sudoku the Giant")
 selection = int(input("Choose a Problem to solve: "))
 
 if selection == 1:
   size = int(input("Choose a number of queens to solve for: "))
-  createBoard(size)
-  tempBoard = queenBoard
-  indices()
-
+  if size <= 250:
+    createBoard(size)
+    board = queenBoard
+    for i range(50000):
+      bestVal = queenHeuristic(board)
+      for j in range(len(board)):
+        tempBoard = board
+        for k in range(len(board)):
+          tempBoard[j] = k
+          currentVal = queenHeuristic(tempBoard)
+          if currentVal < val:
+            
 print(queenBoard)
-print(indexList)
-
-queenHeuristic(indexList)
